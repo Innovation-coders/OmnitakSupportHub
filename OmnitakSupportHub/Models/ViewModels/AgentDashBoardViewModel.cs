3924bd29069183e11e32fa45fd8b76764110664d
Omphemetse Tsita <Omphemetsetsita@yahoo.com>
2025-07-27 15:42:05 +0200

    added a Support Agent DashBoard

---
+++ OmnitakSupportHub/Models/ViewModels/AgentDashBoardViewModel.cs
@@ -0,0 +1,12 @@
+﻿using OmnitakSupportHub.Models;
+
+namespace OmnitakSupportHub.Models.ViewModels
+{
+    public class AgentDashboardViewModel
+    {
+        public string AgentName { get; set; }
+        public string TeamName { get; set; }
+        public List<Ticket> AssignedTickets { get; set; } = new();
+        public Dictionary<int, List<ChatMessage>> TicketChats { get; set; } = new();
+    }
+}

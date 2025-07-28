0e696bc4967c9f52d2b21ce6b6a040c3b383663e
Omphemetse Tsita <Omphemetsetsita@yahoo.com>
2025-07-29 00:38:16 +0200

    added IT Staff with technical expertise

---
+++ OmnitakSupportHub/Controllers/ITStaffController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OmnitakSupportHub.Models;
+
+namespace OmnitakSupportHub.Controllers
+{
+    public class ITStaffController : Controller
+    {
+        private readonly OmnitakContext _context;
+
+        public ITStaffController(OmnitakContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index() => View(await _context.ITStaff.ToListAsync());
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ITStaff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(staff);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(staff);
+        }
+
+        public async Task<IActionResult> Edit(int id) =>
+            View(await _context.ITStaff.FindAsync(id));
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(ITStaff staff)
+        {
+            _context.Update(staff);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id) =>
+            View(await _context.ITStaff.FindAsync(id));
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var staff = await _context.ITStaff.FindAsync(id);
+            _context.ITStaff.Remove(staff);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}

94acc34835c82930ecdb3c32632866fe1bf98aae
KOM2047 <kabeloofentsemodimoeng@gmail.com>
2025-08-26 15:03:32 +0200

    - Added basic functionality to SupportTeamController - Basic Index Action -Basic Create Action

--- OmnitakSupportHub/Controllers/SupportTeamController.cs
+++ OmnitakSupportHub/Controllers/SupportTeamController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OmnitakSupportHub;
+using OmnitakSupportHub.Models;
 
-namespace OmnitakSupportHub.Controllers
+[Authorize(Roles = "Support Manager, Administrator")]
+public class SupportTeamController : Controller
 {
-    public class SupportTeamController : Controller
+    private readonly OmnitakContext _context;
+    public SupportTeamController(OmnitakContext context) => _context = context;
+
+    public async Task<IActionResult> Index()
     {
-        public IActionResult Index()
+        var teams = await _context.SupportTeams.Include(t => t.Users).ToListAsync();
+        return View(teams);
+    }
+
+    public IActionResult Create() => View();
+
+    [HttpPost]
+    public async Task<IActionResult> Create(SupportTeam team)
+    {
+        if (ModelState.IsValid)
         {
-            return View();
+            _context.SupportTeams.Add(team);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
+        return View(team);
     }
 }

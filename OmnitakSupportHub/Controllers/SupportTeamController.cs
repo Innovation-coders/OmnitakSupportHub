2c6a7e5e124be85ddc77adf0008db790bfae4b8e
KOM2047 <kabeloofentsemodimoeng@gmail.com>
2025-08-26 15:02:06 +0200

    - Scaffolded SupportTeamController

---
+++ OmnitakSupportHub/Controllers/SupportTeamController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OmnitakSupportHub.Controllers
+{
+    public class SupportTeamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
